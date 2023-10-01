27fe2a94d4528da79d3775915d49363aa48233b0
NairiGy <nairi.gyulbudaghyan@gmail.com>
2023-10-02 00:12:51 +0300

    add even game src file

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,35 +1,4 @@
 #!/usr/bin/env node
-import readline from "readline-sync";
+import brainEvenGame from '../src/brain-even-game';
 
-const randomInt = (max=100) => Math.floor(Math.random() * max);
-const isEven = (int) => int % 2 === 0;
-
-const welcomeMsg = `Welcome to the Brain Games!
-May I Have your name? `;
-const name = readline.question(welcomeMsg);
-console.log(`Hello, ${name}!`);
-const gameDescr = 'Answer "yes" if the number is even, otherwise answer "no".';
-console.log(gameDescr);
-const numberOfQuestions = 3;
-const successResponse = 'Correct!';
-const failureResponse = (givenAnswer, correctAnswer) => { 
-    return `${givenAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.
-Let's try again, ${name}`;
-    };
-const congratsMsg = `Congratulations, ${name}`;
-for (let i = 0; i < numberOfQuestions; i += 1) {
-    const number = randomInt();
-    const question = `Question: ${number}
-Your asnwer: `;
-    const givenAnswer = readline.question(question);
-    const correctAnswer = isEven(number) ? 'yes' : 'no';
-
-    if (givenAnswer === correctAnswer && i === 2) {
-        console.log(successResponse + '\n' + congratsMsg);
-    } else if (givenAnswer === correctAnswer) {
-        console.log(successResponse);
-    } else {
-        console.log(failureResponse(givenAnswer, correctAnswer));
-        break;
-    }
-}
+brainEvenGame();
---
+++ src/brain-even-game.js
@@ -0,0 +1,41 @@
+import readline from "readline-sync";
+
+const randomInt = (max=100) => Math.floor(Math.random() * max);
+const isEven = (int) => int % 2 === 0;
+const welcomeMsg = `Welcome to the Brain Games!
+May I Have your name? `;
+const gameDescr = 'Answer "yes" if the number is even, otherwise answer "no".';
+const successResponse = 'Correct!';
+const failureResponse = (givenAnswer, correctAnswer, name) => { 
+    return `${givenAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}.
+Let's try again, ${name}`;
+    };
+const question = (number) => {
+    return `Question: ${number}
+Your asnwer: `;
+};
+const numberOfQuestions = 3;    
+
+const brainEvenGame = () => {
+    const name = readline.question(welcomeMsg);
+    console.log(`Hello, ${name}!`);
+    console.log(gameDescr);
+
+    const congratsMsg = `Congratulations, ${name}`;
+    for (let i = 0; i < numberOfQuestions; i += 1) {
+        const number = randomInt();
+        const givenAnswer = readline.question(question(number));
+        const correctAnswer = isEven(number) ? 'yes' : 'no';
+
+        if (givenAnswer === correctAnswer && i === 2) {
+            console.log(successResponse + '\n' + congratsMsg);
+        } else if (givenAnswer === correctAnswer) {
+            console.log(successResponse);
+        } else {
+            console.log(failureResponse(givenAnswer, correctAnswer, name));
+            break;
+        }
+    }
+}
+
+export default brainEvenGame();

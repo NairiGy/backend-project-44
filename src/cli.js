2558325a936a6b030584c78b7675a31667c538de
NairiGy <nairi.gyulbudaghyan@gmail.com>
2023-10-03 20:46:35 +0300

    fix cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
 import readline from 'readline-sync';
 
 const cli = () => {
-  console.log('Welcome to the Brain Games!');
-  const query = 'May I Have your name? ';
+  const query = `Welcome to the Brain Games!
+May I have your name? `;
   const name = readline.question(query);
   console.log(`Hello, ${name}!`);
 };
